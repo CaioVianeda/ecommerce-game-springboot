cc5db3aaca4ef88e88a490d9256c68146844a725
Caio Jesus Vianeda <caio.vianeda@MJV.net>
2022-05-10 21:26:19 -0300

    Iniciado projeto springboot e maven

---
+++ src/main/java/com/mjv/ecommercegame/EcommerceGameApplication.java
@@ -0,0 +1,13 @@
+package com.mjv.ecommercegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjv/ecommercegame/EcommerceGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjv.ecommercegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
