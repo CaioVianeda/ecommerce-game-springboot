365283a6725bb7643a48389f0e2ecbf57823e31c
Caio Vianeda <caio.vianwda@gmail.com>
2022-05-11 16:07:52 -0300

    Adicionado camada de serviço e resgistro de componentes

--- src/main/java/com/mjv/ecommercegame/repositories/ClienteRepository.java
+++ src/main/java/com/mjv/ecommercegame/repositories/ClienteRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mjv.ecommercegame.entities.Cliente;
 
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
 }
--- src/main/java/com/mjv/ecommercegame/resources/ClienteResource.java
+++ src/main/java/com/mjv/ecommercegame/resources/ClienteResource.java
@@ -1,21 +1,34 @@
 package com.mjv.ecommercegame.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mjv.ecommercegame.entities.Cliente;
-import com.mjv.ecommercegame.model.Endereco;
+import com.mjv.ecommercegame.services.ClienteService;
 
 @RestController
 @RequestMapping(value = "/clientes")
 public class ClienteResource {
 	
+	@Autowired
+	private ClienteService service;
+	
 	@GetMapping
-	public ResponseEntity<Cliente> findAll(){
-		Endereco endereco = new Endereco("Rua Badi do Carmo Mesquita", "6", "Praia de Leste", "83255-000");
-		Cliente c = new Cliente(1L, "Caio", "122.875.409-87", "(41)98420-6429", "caio.vianwda@gmail.com","gghh125690", endereco );
-		return ResponseEntity.ok().body(c);
+	public ResponseEntity<List<Cliente>> findAll(){
+		
+		List<Cliente> list = service.findAll(); 
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Cliente> findById(@PathVariable Long id){
+		Cliente obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
