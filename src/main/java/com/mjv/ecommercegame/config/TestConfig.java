6cc2c4232f9d68f2075f61118cb37286ae1719d2
Caio Vianeda <caio.vianwda@gmail.com>
2022-05-12 20:16:37 -0300

    Implementado entidade Produto

--- src/main/java/com/mjv/ecommercegame/config/TestConfig.java
+++ src/main/java/com/mjv/ecommercegame/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.mjv.ecommercegame.entities.Cliente;
 import com.mjv.ecommercegame.entities.Pedido;
+import com.mjv.ecommercegame.entities.Produto;
 import com.mjv.ecommercegame.entities.enums.StatusPedido;
 import com.mjv.ecommercegame.model.Endereco;
 import com.mjv.ecommercegame.repositories.ClienteRepository;
 import com.mjv.ecommercegame.repositories.PedidoRepository;
+import com.mjv.ecommercegame.repositories.ProdutoRepository;
 
 @Configuration
 @Profile("test")
@@ -25,19 +27,27 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Produto pr1 = new Produto(null,200.00,"Alexa1");
+		Produto pr2 = new Produto(null,300.00,"Alexa2");
+		Produto pr3 = new Produto(null,400.00,"Alexa3");
+		produtoRepository.saveAll(Arrays.asList(pr1,pr2,pr3));
 		
 		Endereco endereco = new Endereco("Rua Badi do Carmo Mesquita", "6", "Praia de Leste", "83255-000");
 		Cliente c1 = new Cliente(null, "Caio", "122.875.409-87", "(41)98420-6429", "caio.vianwda@gmail.com","gghh125690", endereco);
 		Cliente c2 = new Cliente(null, "Monalisa", "122.721.875-00", "(41)98420-6429", "monalisa@gmail.com","gghh125690", endereco);
+		clienteRepository.saveAll(Arrays.asList(c1,c2));
 		
 		Pedido p1 = new Pedido(null, Instant.parse("2022-06-20T21:53:07Z"), StatusPedido.AGUARDANDO_PAGAMENTO,c1);
 		Pedido p2 = new Pedido(null, Instant.parse("2022-08-20T21:35:07Z"), StatusPedido.AGUARDANDO_PAGAMENTO,c2);
 		Pedido p3 = new Pedido(null, Instant.parse("2022-07-20T21:28:07Z"), StatusPedido.AGUARDANDO_PAGAMENTO,c1);
-		
-		clienteRepository.saveAll(Arrays.asList(c1,c2));
 		pedidoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 	}
 	
 }
---
+++ src/main/java/com/mjv/ecommercegame/entities/Produto.java
@@ -0,0 +1,75 @@
+package com.mjv.ecommercegame.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Double valor;
+	private String descricao;
+	
+	public Produto() {
+		
+	}
+
+	public Produto(Long id, Double valor, String descricao) {
+		super();
+		this.id = id;
+		this.valor = valor;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+
+}
