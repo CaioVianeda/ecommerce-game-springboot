e822c212dd2a25a4212a556d2bb48756fa0e9b58
Caio Vianeda <caio.vianwda@gmail.com>
2022-05-19 15:07:07 -0300

    Implementado inclusão, exclusão e alteração de produtos

--- src/main/java/com/mjv/ecommercegame/services/ProdutoService.java
+++ src/main/java/com/mjv/ecommercegame/services/ProdutoService.java
@@ -3,11 +3,15 @@ package com.mjv.ecommercegame.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mjv.ecommercegame.entities.Endereco;
 import com.mjv.ecommercegame.entities.Produto;
 import com.mjv.ecommercegame.repositories.ProdutoRepository;
+import com.mjv.ecommercegame.services.excecoes.ResourceNotFoundException;
 
 
 @Service
@@ -23,5 +27,32 @@ public class ProdutoService {
 	public Produto findById(Long id) {
 		Optional<Produto> obj =  repository.findById(id);
 		return obj.get();
+	}
+	
+	public Produto insert(Produto obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public Produto update(Long id, Produto obj) {
+		
+		try {
+		Produto entity = repository.getById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Produto entity, Produto obj) {
+		
+		entity.setDescricao(obj.getDescricao());
+		entity.setPlataforma(obj.getPlataforma());
+		entity.setValor(obj.getValor());
+		
 	}
 }
